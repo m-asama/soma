a2d861f32bb154cd397c96fa552883c17e572bba
Masakazu Asama <m-asama@ginzado.co.jp>
2016-12-02 09:56:05 +0900

    std の型と合わせるため int を signed int に変更。

--- kernel/type.h
+++ kernel/type.h
@@ -7,13 +7,13 @@
 #pragma once
 
 typedef unsigned long	uint64_t;
-typedef long		int64_t;
+typedef signed long	int64_t;
 typedef unsigned int	uint32_t;
-typedef int		int32_t;
+typedef signed int	int32_t;
 typedef unsigned short	uint16_t;
-typedef short		int16_t;
+typedef signed short	int16_t;
 typedef unsigned char	uint8_t;
-typedef char		int8_t;
+typedef signed char	int8_t;
 
 typedef uint64_t	size_t;
 
